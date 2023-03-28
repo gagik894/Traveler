611268b1775baf29d7deed06816a37ccecc6b574
gagik894 <gagiksargsyan894@gmail.com>
2023-03-28 11:26:22 +0400

    Improved bottom navigation

--- app/src/main/java/com/together/traveler/MainActivity.java
+++ app/src/main/java/com/together/traveler/MainActivity.java
@@ -1,19 +1,18 @@
 package com.together.traveler;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity
 {
-    BottomNavigationView bottomNavigationView;
-    FragmentManager fragmentManager;
-    FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,40 +22,16 @@ public class MainActivity extends AppCompatActivity
             getSupportActionBar().hide();
         }
 
-        bottomNavigationView = findViewById(R.id.nvMain);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = Objects.requireNonNull(navHostFragment).getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.nvMain);
+        NavigationUI.setupWithNavController(bottomNav, navController);
 
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            Log.i("LKJ", "onNavigationItemSelected: ");
-            fragmentManager = getSupportFragmentManager();
-            transaction = fragmentManager.beginTransaction();
-            transaction.setReorderingAllowed(true);
-            try {
-                int itemId = item.getItemId();
-                if (itemId == R.id.action_home) {
-                    transaction.replace(R.id.viewFragment, HomeFragment.class, null);
-                } else if (itemId == R.id.action_search) {
-                    transaction.replace(R.id.viewFragment, EventFragment.class, null);
-                } else if (itemId == R.id.action_add) {
-                    transaction.replace(R.id.viewFragment, BlankFragment.class, null);
-                }else if (itemId == R.id.action_map) {
-                    transaction.replace(R.id.viewFragment, MapFragment.class, null);
-                }else if (itemId == R.id.action_user) {
-                    transaction.replace(R.id.viewFragment, UserSelfFragment.class, null);
-                }
-
-                transaction.commit();
-            } catch (Exception e) {
-                Log.e("LKJ", "onCreate: ", e);
-            }
-            return false;
-        });
-        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
     }
 
 }
